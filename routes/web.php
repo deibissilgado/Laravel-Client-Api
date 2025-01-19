1f1cf5ddbba53e9c9a323cab7910f4a81fa48a4a
deibissilgado <davidsilgado@gmail.com>
2025-01-19 18:20:37 -0500

    Agregar ClientesController y modificar la ruta principal para obtener datos de la API

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use GuzzleHttp\Client;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClientesController;
+use Illuminate\Support\Facades\Http; // para usar  Guzzle HTTP client
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', ClientesController::class);
