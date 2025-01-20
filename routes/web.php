9f740e18738c23562163e43bb94f0bf864d63174
deibissilgado <davidsilgado@gmail.com>
2025-01-19 19:30:18 -0500

    Agregar rutas para editar, ver y eliminar clientes en ClientesController

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,6 @@ use Illuminate\Support\Facades\Http; // para usar  Guzzle HTTP client
 
 
 Route::get('/', [ClientesController::class, 'index']);
+Route::get('/Editar/{id}', [ClientesController::class, 'edit']);
+Route::get('/Ver/{id}', [ClientesController::class, 'show']);
+Route::get('/Eliminar/{id}', [ClientesController::class, 'destroy']);
